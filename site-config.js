8a760fd815ae5c4dd11c86b1d966c9b1d03a5711
Gonza <gonzalososa342@gmail.com>
2024-06-21 06:15:54 -0300

    Initial site config file

---
+++ site-config.js
@@ -0,0 +1,11 @@
+export const SITE = {
+  TITLE: 'Manga Reader',
+  DEFAULT_LANG: 'en',
+  tagline: 'Manga Reader',
+  url: 'https://manga-reader.vercel.app',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'mangadex',
+}
