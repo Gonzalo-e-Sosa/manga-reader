147bc07c96dcb1634478d23bf70ebd86d29438a5
Gonza <gonzalososa342@gmail.com>
2024-06-22 18:49:31 -0300

    Fetching data to MangaDex

--- src/services/fetchData.ts
+++ src/services/fetchData.ts
@@ -1,29 +1,29 @@
 // get chapter image from an chapter id
 // dataSaver contains the id of the image compressed
 
-import { URL_MANGA, UPLOADS_BASE_URL, URL_CHAPTER_IMAGES, URL_COVER_ART_IMAGE, URL_AUTHOR, URL_COVER_ART, URL_MANGA_RANDOM } from "@/consts";
-import type { CoverResponse, ThumbnailSize, ChapterResponse, MangaResponse } from "./types";
+import { ENDPOINTS } from "@/consts";
+import type { CoverResponse, ThumbnailSize, ChapterResponse, MangaResponse, MangaListResponse, ListOptions } from "./types";
 
 export const getMangaData = async (id: string): Promise<MangaResponse> => {
-  const res = await fetch(`${URL_MANGA}/${id}`);
+  const res = await fetch(`${ENDPOINTS.MANGA}/${id}`);
 
   return (await res.json());
 }
 
 export const getMangaRandomData = async (): Promise<MangaResponse> => {
-  const res = await fetch(URL_MANGA_RANDOM);
+  const res = await fetch(ENDPOINTS.MANGA_RANDOM);
 
   return (await res.json());
 }
 
 export const getAuthorData = async (id: string) => {
-  const res = await fetch(`${URL_AUTHOR}/${id}`);
+  const res = await fetch(`${ENDPOINTS.AUTHOR}/${id}`);
 
   return (await res.json());
 }
 
 export const getCoverArtData = async (id: string): Promise<CoverResponse> => {
-  const res = await fetch(`${URL_COVER_ART}/${id}`);
+  const res = await fetch(`${ENDPOINTS.COVER_ART}/${id}`);
 
   return (await res.json());
 }
@@ -38,14 +38,14 @@ export const getCoverArtFilename = (res: CoverResponse) => {
 export const getCoverArtImage = (mangaId: string, coverArtFilename: string, thumbnailSize?: ThumbnailSize) => {
   const width = thumbnailSize ? `.${thumbnailSize}.jpg` : '';
 
-  return `${URL_COVER_ART_IMAGE}/${mangaId}/${coverArtFilename}${width}`;
+  return `${ENDPOINTS.COVER_ART_IMAGE}/${mangaId}/${coverArtFilename}${width}`;
 }
 
 
 // chapters images
 export const getChapterImagesData = async (chapterId: string): Promise<ChapterResponse> => {
   const res = await fetch(
-    `${URL_CHAPTER_IMAGES}/${chapterId}`
+    `${ENDPOINTS.CHAPTER_IMAGES}/${chapterId}`
   );
 
   return (await res.json());
@@ -53,8 +53,45 @@ export const getChapterImagesData = async (chapterId: string): Promise<ChapterRe
 
 export const getPageImage = async (hash: string, pageId: string, dataSaver = true) => {
   const image = await fetch(
-    `${UPLOADS_BASE_URL}/${dataSaver ? 'dataSaver' : 'data'}/${hash}/${pageId}`
+    `${ENDPOINTS.UPLOADS}/${dataSaver ? 'dataSaver' : 'data'}/${hash}/${pageId}`
   )
   return image;
 }
 
+/*
+`https://api.mangadex.org/manga?includes[]=cover_art&includes[]=artist&includes[]=author&order[followedCount]=desc&contentRating[]=safe&hasAvailableChapters=true&createdAtSince=${encodeURIComponent(dateTime)`
+*/
+
+export const createMangaListURL = (options?: ListOptions, baseURL = ENDPOINTS.MANGA_LIST) => {
+  const INCLUDES = 'includes[]';
+  const url = new URL(baseURL);
+
+  if (!options) return url.toString();
+
+  const { limit, offset, contentRating, createdAtSince, updatedAtSince, coverArt, artist, author, order } = options;
+
+  if (limit) url.searchParams.append(`limit`, limit.toString());
+
+  if (offset) url.searchParams.append('offset', offset.toString());
+
+  if (coverArt) url.searchParams.append(INCLUDES, 'cover_art');
+
+  if (artist) url.searchParams.append(INCLUDES, 'artist');
+
+  if (author) url.searchParams.append(INCLUDES, 'author');
+
+  if (order) url.searchParams.append('order[followedCount]', 'desc');
+
+  if (contentRating) url.searchParams.append("contentRating[]", contentRating);
+
+  if (createdAtSince) url.searchParams.append('createdAtSince', createdAtSince.toISOString().slice(0, -5));
+
+  if (updatedAtSince) url.searchParams.append('updatedAtSince', updatedAtSince.toISOString().slice(0, -5));
+
+  return url.toString();
+}
+
+export const getMangaList = async (options?: ListOptions): Promise<MangaListResponse> => {
+  const res = await fetch(createMangaListURL(options));
+  return (await res.json());
+}
