32559af68791741661e76148d352eb9c9b7bb246
Gonza <gonzalososa342@gmail.com>
2024-06-22 18:50:08 -0300

    Endpoints to MangaDex

--- src/consts.js
+++ src/consts.js
@@ -1,10 +1,3 @@
-// Manga List returns 10 results by default limit
-// Manga List return cover_art and artist in the results
-
-export const ENDPOINTS = {
-  MANGA_LIST_CREATED_AT: dateTime => `https://api.mangadex.org/manga?includes[]=cover_art&includes[]=artist&includes[]=author&order[followedCount]=desc&contentRating[]=safe&hasAvailableChapters=true&createdAtSince=${encodeURIComponent(dateTime)}`
-};
-
 export const API_BASE_URL = 'https://api.mangadex.org';
 export const UPLOADS_BASE_URL = 'https://uploads.mangadex.org';
 
@@ -14,3 +7,14 @@ export const URL_AUTHOR = `${API_BASE_URL}/author`;
 export const URL_COVER_ART = `${API_BASE_URL}/cover`;
 export const URL_COVER_ART_IMAGE = `${UPLOADS_BASE_URL}/covers`;
 export const URL_CHAPTER_IMAGES = `${API_BASE_URL}/at-home/server`;
+
+export const ENDPOINTS = {
+  MANGA: URL_MANGA,
+  MANGA_LIST: URL_MANGA,
+  MANGA_RANDOM: URL_MANGA_RANDOM,
+  AUTHOR: URL_AUTHOR,
+  COVER_ART: URL_COVER_ART,
+  COVER_ART_IMAGE: URL_COVER_ART_IMAGE,
+  CHAPTER_IMAGES: URL_CHAPTER_IMAGES,
+  UPLOADS: UPLOADS_BASE_URL
+}
