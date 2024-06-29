c101fb283fd6338b534797556a872e67818952bd
Gonza <gonzalososa342@gmail.com>
2024-06-29 02:52:23 -0300

    Added image to optimize images

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,17 @@
 import { defineConfig } from 'astro/config';
 import htmx from "astro-htmx";
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  integrations: [htmx()]
+  integrations: [htmx(), tailwind()],
+  image: {
+    remotePatterns: [{
+      protocol: 'https',
+      hostname: 'uploads.mangadex.org',
+      pathname: '/covers/**'
+    }]
+  }
 });
