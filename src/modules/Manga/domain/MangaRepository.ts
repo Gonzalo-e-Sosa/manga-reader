399e777f320d771e80995a78f71f9450a2291c32
Gonza <gonzalososa342@gmail.com>
2024-07-03 21:21:35 -0300

    Functions declaration for uses cases of manga

---
+++ src/modules/Manga/domain/MangaRepository.ts
@@ -0,0 +1,23 @@
+import type { MangaListOptions } from "@/services/types";
+import type { Manga } from "./Manga"
+
+export interface MangaRepository {
+  getById(id: string): Promise<Manga>;
+  getSingleBySimilarTitle(similarTitle: string): Promise<Manga>;
+  getSingleByStatus(status: string): Promise<Manga>;
+  getSingleByYear(year: Date): Promise<Manga>;
+  getSingleByCreatedAt(date: Date): Promise<Manga>;
+  getSingleByUpdatedAt(date: Date): Promise<Manga>;
+  getSingleByLatestUpdatedChapter(): Promise<Manga>;
+  getSingleByContentRating(contentRating: string): Promise<Manga>;
+
+  getCollectionBySimilarTitle(similarTitle: string): Promise<Manga[]>;
+  getCollectionByStatus(status: string): Promise<Manga[]>;
+  getCollectionByYear(year: Date): Promise<Manga[]>;
+  getCollectionByCreatedAt(date: Date): Promise<Manga[]>;
+  getCollectionByUpdatedAt(date: Date): Promise<Manga[]>;
+  getCollectionByLatestUpdatedChapter(): Promise<Manga[]>;
+  getCollectionByContentRating(contentRating: string): Promise<Manga[]>;
+
+  getAll(options: MangaListOptions): Promise<Manga[]>
+}
