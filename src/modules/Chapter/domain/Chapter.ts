135ee12becd2e5d31eebcf1d491e14e171e8fbb8
Gonza <gonzalososa342@gmail.com>
2024-07-05 19:12:53 -0300

    Added chapter files for hexagonal architecture

---
+++ src/modules/Chapter/domain/Chapter.ts
@@ -0,0 +1,13 @@
+export interface Chapter {
+  volume: string
+  chapter: string
+  title?: string
+  translatedLanguage: string
+  externalUrl?: string
+  publishAt: string
+  readableAt: string
+  createdAt: string
+  updatedAt: string
+  pages: number
+  version: number
+}
