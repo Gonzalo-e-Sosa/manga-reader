13a7e2845e32187b52eff22498cf429414ee9cb7
Gonza <gonzalososa342@gmail.com>
2024-07-05 19:18:01 -0300

    Include cover art in the fetching for list and artist, author and cover art for single

--- src/modules/Manga/infrastructure/FetchMangaRepository.ts
+++ src/modules/Manga/infrastructure/FetchMangaRepository.ts
@@ -1,84 +1,90 @@
 import { mapMangaDataSingle, mapMangaDataCollection } from '@/utils/map';
 import type { MangaRepository } from '../domain/MangaRepository';
 import { getMangaData, getMangaList } from '@/services/fetchData';
-import { ensureIsValidContentRating, ensureIsValidStatus } from '../domain/Manga';
+import { ensureIsValidContentRating, ensureIsValidStatus, type Manga } from '../domain/Manga';
+import type { MangaOptions } from '@/services/types';
 
+/**
+ * Creates a new instance of the MangaRepository.
+ *
+ * @return {MangaRepository} The created MangaRepository instance.
+ */
 export const createFetchMangaRepository = (): MangaRepository => {
   return {
-    getById: async (id: string) => {
-      const { data } = await getMangaData(id);
+    getById: async (id: string, options?: MangaOptions): Promise<Manga> => {
+      const { data } = await getMangaData(id, { ...options, author: 'author', artist: 'artist', coverArt: 'cover_art' });
       return mapMangaDataSingle(data);
     },
-    getSingleBySimilarTitle: async (similarTitle: string) => {
-      const { data } = await getMangaList({ title: similarTitle, limit: 1, includes: { coverArt: true } })
+    getSingleBySimilarTitle: async (similarTitle: string): Promise<Manga> => {
+      const { data } = await getMangaList({ title: similarTitle, limit: 1, includes: { coverArt: 'cover_art' } })
       const [manga] = data;
       return mapMangaDataSingle(manga);
     },
-    getSingleByYear: async (year: Date) => {
-      const { data } = await getMangaList({ year, limit: 1, includes: { coverArt: true } });
+    getSingleByYear: async (year: Date): Promise<Manga> => {
+      const { data } = await getMangaList({ year, limit: 1, includes: { coverArt: 'cover_art' } });
       const [manga] = data;
       return mapMangaDataSingle(manga);
     },
-    getSingleByStatus: async (status: string) => {
+    getSingleByStatus: async (status: string): Promise<Manga> => {
       const validStatus = ensureIsValidStatus(status);
-      const { data } = await getMangaList({ status: validStatus, limit: 1, includes: { coverArt: true } });
+      const { data } = await getMangaList({ status: validStatus, limit: 1, includes: { coverArt: 'cover_art' } });
       const [manga] = data;
       return mapMangaDataSingle(manga);
     },
-    getSingleByCreatedAt: async (date: Date) => {
-      const { data } = await getMangaList({ createdAtSince: date, limit: 1, includes: { coverArt: true } });
+    getSingleByCreatedAt: async (date: Date): Promise<Manga> => {
+      const { data } = await getMangaList({ createdAtSince: date, limit: 1, includes: { coverArt: 'cover_art' } });
       const [manga] = data;
       return mapMangaDataSingle(manga);
     },
-    getSingleByUpdatedAt: async (date: Date) => {
-      const { data } = await getMangaList({ updatedAtSince: date, limit: 1, includes: { coverArt: true } });
+    getSingleByUpdatedAt: async (date: Date): Promise<Manga> => {
+      const { data } = await getMangaList({ updatedAtSince: date, limit: 1, includes: { coverArt: 'cover_art' } });
       const [manga] = data;
       return mapMangaDataSingle(manga);
     },
-    getSingleByLatestUpdatedChapter: async () => {
-      const { data } = await getMangaList({ limit: 1, includes: { coverArt: true } });
+    getSingleByLatestUpdatedChapter: async (): Promise<Manga> => {
+      const { data } = await getMangaList({ limit: 1, includes: { coverArt: 'cover_art' } });
       const [manga] = data;
       return mapMangaDataSingle(manga);
     },
-    getSingleByContentRating: async (contentRating: string) => {
+    getSingleByContentRating: async (contentRating: string): Promise<Manga> => {
       const validContentRating = ensureIsValidContentRating(contentRating);
-      const { data } = await getMangaList({ contentRating: validContentRating, limit: 1, includes: { coverArt: true } });
+      const { data } = await getMangaList({ contentRating: validContentRating, limit: 1, includes: { coverArt: 'cover_art' } });
       const [manga] = data;
       return mapMangaDataSingle(manga);
     },
 
-    getCollectionBySimilarTitle: async (similarTitle: string) => {
-      const { data: mangas } = await getMangaList({ title: similarTitle, includes: { coverArt: true } })
+    getCollectionBySimilarTitle: async (similarTitle: string): Promise<Manga[]> => {
+      const { data: mangas } = await getMangaList({ title: similarTitle, includes: { coverArt: 'cover_art' } })
       return mapMangaDataCollection(mangas);
     },
-    getCollectionByYear: async (year: Date) => {
-      const { data: mangas } = await getMangaList({ year, includes: { coverArt: true } })
+    getCollectionByYear: async (year: Date): Promise<Manga[]> => {
+      const { data: mangas } = await getMangaList({ year, includes: { coverArt: 'cover_art' } })
       return mapMangaDataCollection(mangas);
     },
-    getCollectionByStatus: async (status: string) => {
+    getCollectionByStatus: async (status: string): Promise<Manga[]> => {
       const validStatus = ensureIsValidStatus(status);
-      const { data: mangas } = await getMangaList({ status: validStatus, includes: { coverArt: true } })
+      const { data: mangas } = await getMangaList({ status: validStatus, includes: { coverArt: 'cover_art' } })
       return mapMangaDataCollection(mangas);
     },
-    getCollectionByCreatedAt: async (date: Date) => {
-      const { data: mangas } = await getMangaList({ createdAtSince: date, includes: { coverArt: true } })
+    getCollectionByCreatedAt: async (date: Date): Promise<Manga[]> => {
+      const { data: mangas } = await getMangaList({ createdAtSince: date, includes: { coverArt: 'cover_art' } })
       return mapMangaDataCollection(mangas);
     },
-    getCollectionByUpdatedAt: async (date: Date) => {
-      const { data: mangas } = await getMangaList({ updatedAtSince: date, includes: { coverArt: true } })
+    getCollectionByUpdatedAt: async (date: Date): Promise<Manga[]> => {
+      const { data: mangas } = await getMangaList({ updatedAtSince: date, includes: { coverArt: 'cover_art' } })
       return mapMangaDataCollection(mangas);
     },
-    getCollectionByLatestUpdatedChapter: async () => {
+    getCollectionByLatestUpdatedChapter: async (): Promise<Manga[]> => {
       const { data: mangas } = await getMangaList()
       return mapMangaDataCollection(mangas);
     },
-    getCollectionByContentRating: async (contentRating: string) => {
+    getCollectionByContentRating: async (contentRating: string): Promise<Manga[]> => {
       const validContentRating = ensureIsValidContentRating(contentRating);
-      const { data: mangas } = await getMangaList({ contentRating: validContentRating, includes: { coverArt: true } })
+      const { data: mangas } = await getMangaList({ contentRating: validContentRating, includes: { coverArt: 'cover_art' } })
       return mapMangaDataCollection(mangas);
     },
-    getAll: async (options) => {
-      const { data: mangas } = await getMangaList(options);
+    getAll: async (options): Promise<Manga[]> => {
+      const { data: mangas } = await getMangaList({ ...options, includes: { coverArt: 'cover_art' } });
       return mapMangaDataCollection(mangas);
     }
   }
